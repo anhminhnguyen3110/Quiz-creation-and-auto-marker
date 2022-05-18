04543632baf3ff02d965d3abf8bbd6b81bdb6876
Khoa Pham <khoaphamsut@gmail.com>
2022-05-18 11:20:56 +1000

    fix phpenhanced

--- phpenhancements.php
+++ phpenhancements.php
@@ -47,21 +47,21 @@
         <?php
             echo "<section class='item'>
                     <h2>Student login and register system</h2>
-                    <p>The goal of this system is to store the firstname, lastname and student_id of student in a new table called student that is required so with a student id, there can be only one consistent firstname and lastname.
-                    This also includes other enhancement, that is normalising the attempt table and create a primary key for that, foreign key for students table and link them together</p>
-                    <p>There is a site for register so student can register their information to start doing the quiz, as well as a login, link here <a href='logoutEnhancements.php'>login & register</a></p>
+                    <p>This system is created to store the first name, last name, and ID of students in a new table called students. It will require users to register before they do the quiz; therefore, each user only has one consistent account including their first name, last name, and ID.
+                    Also, they can log in again with their account to continue doing the quiz or see their result on the status page. Furthermore, the system acts as a precursor to the enhancement of normalizing the attempt table. In particular, a primary-foreign key between attempts and students tables is implemented to link the data between them.</p>
+                    <p>Click on the link here <a href='logoutEnhancements.php'>login & register</a> to move to the page.</p>
                 </section>";
 
             echo "<section class='item'>
                 <h2>Admin login and security handler</h2>
-                <p>The goal of this enhancement is to create a table storing username and password to access manage.php, if an user wrongly type password three times, the access to that account will be blocked for 5 miniutes until it can be log in again.</p>
-                <p>There is a site for supervisor login to access manage.php, link here <a href='logoutAminEnhancements.php'>Supervisor login</a></p>
+                <p>To enable more secure access to the supervisor page, a security handler is created. In other words, if a user types a password incorrectly three times, the access to that account will be blocked for 5 minutes until it can be logged in again. Additionally, we create a table to store usernames and passwords accessing admin login to keep track of the time of wrong passwords.</p>
+                <p>To access to admin login, click here: <a href='logoutAminEnhancements.php'>Supervisor login</a></p>
             </section>";
 
             echo "<section class='item'>
                 <h2>Admin sort option</h2>
-                <p>This enhancement allows admin to sort the table according to criteria such as score, attemptId, firstname and lastname, which make it more easier to arrange data.</p>
-                <p><a href='./manage.php#sortManage'>Table sorting feature</a></p>
+                <p>For the admin to easily follow, arrange, and analyze the data from users, we create sorting functions. The supervisor can sort the table according to different features such as score, attempt ID, first name, and last name, making it easier to manage the data. </p>
+                <p>To gain a closer insight into the enhancement, go to this link: <a href='./manage.php#sortManage'> Table sorting feature</a></p>
             </section>";
 
         ?>
