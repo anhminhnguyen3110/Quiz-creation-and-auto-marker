159ef2d26e4aa793093c8788a5c34342720951d0
Sotatek-MinhNguyen <minh.nguyen8@sotatek.com>
2022-05-27 13:54:16 +1000

    fix comment

--- enhancements.php
+++ enhancements.php
@@ -1,3 +1,14 @@
+<!--
+PHP file for a COS10026 Project by React Lions
+Author: 
+Duy Khoa Pham (103515617)
+Gurmehar Singh (103510447)
+Anh Minh Nguyen (103178955)
+Bradley Bowering (102673815)
+Andrew Moutsos(103982376)
+Purpose: logout enhancement
+last modified: 27.05.2022 
+ -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
